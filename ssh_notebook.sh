72247ccc533e06bd6b4b18590f6ba8784fc8f416
Jakub Sękowski <sequba@gmail.com>
2017-03-04 14:36:59 +0100

    More remote connection scripts
    Signed-off-by: Jakub Sękowski <sequba@gmail.com>

--- ssh_notebook.sh
+++ ssh_notebook.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+
 p=9876
+c=$(( $RANDOM % 9 + 1 ))
+echo "port=$p, computer=$c"
 ssh -L $p:localhost:$p tryglaw \
 	-t ssh -L $p:localhost:$p hera \
-	-t ssh -L $p:localhost:$p lab110-01
+	-t ssh -L $p:localhost:$p lab110-0$c
