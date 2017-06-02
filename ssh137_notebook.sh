633bfc4c4e57a33771d71eaeac65e76e5384909e
sequba <sequba@gmail.com>
2017-06-02 11:47:59 +0200

    Script for ssh to lab 137
    Signed-off-by: sequba <sequba@gmail.com>

---
+++ ssh137_notebook.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+p=9876
+c=$(( $RANDOM % 9 + 1 ))
+echo "port=$p, computer=$c"
+ssh -L $p:localhost:$p tryglaw \
+	-t ssh -L $p:localhost:$p hera \
+	-t ssh -L $p:localhost:$p lab137-0$c
